cec60ad1982defa18e8d13ba2fe1ebfa9bb1f4b6
Melvin C V <melrockz@gmail.com>
2023-08-15 10:20:03 +0530

    added code and ci file

---
+++ app-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'cloudops-w2-db-cluster.cluster-cf6xjg8izaoi.ap-southeast-1.rds.amazonaws.com',
+    DB_USER : 'melvin',
+    DB_PWD : 'RZe=urGbp4<-"F4T',
+    DB_DATABASE : 'webappdb'
+});
